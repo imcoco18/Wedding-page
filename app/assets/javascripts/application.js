47c0d08b7a7045c8583ca39247a3106cecbc0ec6
mako <coco.jasmine18@icloud.com>
2020-07-21 08:11:49 +0000

    [add]slide2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,40 +17,32 @@
 //= require_tree .
 //= require bootstrap-sprockets
 
-$(document).on('turbolinks:load', function() {
- $('.slick01').slick({
- 		arrows: true,
-    infinite: true, //スライドのループ有効化
-    dots: true, //ドットのナビゲーションを表示
-    slidesToShow: 4, //表示するスライドの数
-    slidesToScroll: 1, //スクロールで切り替わるスライドの数
- });
-});
+$(document).on('turbolinks:load', ()=> {
+    // 省略
 
-// $(document).on('turbolinks:load', function() {
-// 	$('.slick01').skippr({
-//     // スライドショーの変化 ("fade" or "slide")
-//     transition : 'slide',
-//     // 変化に係る時間(ミリ秒)
-//     speed : 1000,
-//     // easingの種類
-//     easing : 'easeOutQuart',
-//     // ナビゲーションの形("block" or "bubble")
-//     navType : 'block',
-//     // 子要素の種類("div" or "img")
-//     childrenElementType : 'div',
-//     // ナビゲーション矢印の表示(trueで表示)
-//     arrows : true,
-//     // スライドショーの自動再生(falseで自動再生なし)
-//     autoPlay : true,
-//     // 自動再生時のスライド切替間隔(ミリ秒)
-//     autoPlayDuration : 3000,
-//     // キーボードの矢印キーによるスライド送りの設定(trueで有効)
-//     keyboardOnAlways : true,
-//     // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
-//     hidePrevious : false
-//   });
-// });
+    $(function() {
+      $('.slick01').slick({
+        prevArrow:'<i class="fa fa-angle-left arrow arrow-left"></i>',
+        nextArrow:'<i class="fa fa-angle-right arrow arrow-right"></i>',
+        autoplay:true,
+        autoplaySpeed:5000,
+        slidesToShow: 4, //表示するスライドの数
+    		slidesToScroll: 1, //スクロールで切り替わるスライドの数
+
+      });
+    });
+
+    $(function() {
+      $('.slick02').slick({
+      	arrows:false,
+        autoplay:true,
+        autoplaySpeed:3000,
+        slidesToShow: 2, //表示するスライドの数
+    		slidesToScroll: 1, //スクロールで切り替わるスライドの数
+
+      });
+    });
+});
 
 $(document).on('turbolinks:load', function() {
  $("#hoge").css("color","red")
